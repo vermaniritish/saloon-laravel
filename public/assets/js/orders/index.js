abf6d5bde1a41392969857f3397ff83b136bd306
Divya Chaudhary <chaudharydivya125@gmail.com>
2024-02-20 21:59:22 +0530

    fix

--- public/assets/js/orders/index.js
+++ public/assets/js/orders/index.js
@@ -10,3 +10,37 @@ function submitForm(statusKey) {
     }
     document.getElementById('filterForm').submit();
 }
+
+async function switch_diary_page_action(url, that) {
+    var flag = $(that).data('value');
+    var currentStatus = $('#Currentstatus').val();
+    if (flag.toLowerCase() === currentStatus) {
+        var text = $(that).closest('.dropdown').find('.btn').text().toLowerCase();
+        set_notification('error', 'Cannot change status to ' + text + ' again.');
+        return;
+    }
+    var statusStyles = await getStatuses();
+    $.ajax({
+        url: url,
+        type: 'post',
+        data: {
+            flag: flag,
+            _token: csrf_token()
+        },
+        success: function (resp) {
+            if ($(that).data('value') && resp.status === 'success') {
+                var buttonText = $(that).text();
+                var statusData = statusStyles[flag];
+                var buttonStyle = statusData.styles || 'background-color: #ffffff; color: #000000;';
+                $(that).closest('.dropdown').find('.btn').removeClass().addClass('btn btn-sm dropdown-toggle').attr('style', buttonStyle).text(buttonText);
+                set_notification('success', 'Order status updated successfully.');
+            }
+        }
+    })
+}
+function getStatuses() {
+    return $.ajax({
+        url: admin_url +'/order/getStatus',
+        method: 'GET',
+    });
+}
