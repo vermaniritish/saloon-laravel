737345b20ad64a19509958a9544266b79eeca98c
Divya Chaudhary <chaudharydivya125@gmail.com>
2024-02-26 05:34:45 +0530

    fix

--- database/migrations/divya_migrations.sql
+++ database/migrations/divya_migrations.sql
@@ -5,3 +5,8 @@ ALTER TABLE `order_status_history`
 ADD `old_value` int NULL AFTER `staff_id`,
 ADD `new_value` tinytext COLLATE 'utf8mb4_general_ci' NULL AFTER `old_value`,
 ADD `field` tinytext COLLATE 'utf8mb4_general_ci' NULL AFTER `new_value`;
+
+ALTER TABLE `order_products`
+ADD `updated_at` datetime NULL ON UPDATE CURRENT_TIMESTAMP;
+ALTER TABLE `order_products`
+CHANGE `created` `created_at` datetime NULL ON UPDATE CURRENT_TIMESTAMP AFTER `deleted_at`;
